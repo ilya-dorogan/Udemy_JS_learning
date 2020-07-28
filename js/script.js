93514ccd6df2cb3d5b1fa30411aa6e560436f61a
Ilya Dorogan <enotshannel@gmail.com>
2020-07-28 22:08:59 +0300

    first_commit

---
+++ js/script.js
@@ -0,0 +1,61 @@
+"use strict";
+
+/*
+let res = confirm("True or false?");
+
+alert(res);
+*/
+
+/*
+let answer = +prompt("Enter your age.", "0");
+
+alert(answer);
+alert(typeof (answer));
+alert(answer + 5);
+*/
+
+/*
+const answers = [];
+
+answers[0] = prompt("Enter your name.", "");
+answers[1] = prompt("Enter your soname.", "");
+answers[2] = prompt("Enter your age.", "");
+answers[3] = prompt("Enter your favorite color.", "");
+answers[4] = prompt("Enter year you was born.", "");
+
+document.write(answers);
+
+alert(typeof (answers[0]));
+*/
+
+/*
+const type = "running";
+const user = "Ilya";
+
+alert('http://url.com/' + type);
+alert(`http://url.com/${type}`);
+alert(`Hello,${user}.`);
+*/
+
+/*
+alert(5 + "juice");
+alert(5 + +"juice");
+alert(5 + +"3");
+*/
+
+let incr = 5,
+	decr = 10;
+
+alert(`Increment = ${incr};`);
+alert(`Decrement = ${decr};`);
+
+//постфиксный
+incr++;
+decr--;
+
+//префиксный
+++incr;
+--decr;
+
+alert(`Increment = ${incr};`);
+alert(`Decrement = ${decr};`);
