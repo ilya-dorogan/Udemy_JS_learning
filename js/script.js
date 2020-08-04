ede94cb5c5a4ef782bd2bd3dd0f69f39bac3c2d6
Ilya Dorogan <enotshannel@gmail.com>
2020-08-04 22:33:58 +0300

    lesoon_27_end

--- js/script.js
+++ js/script.js
@@ -425,3 +425,35 @@ console.log(Boolean('4')); //true
 console.log(!!NaN); //false
 console.log(!!45); //true
 */
+
+/*LESSON 27 Задачи с собеседований на понимание основ*/
+
+/*
+let x = 5;
+alert(x++); // 5 
+
+console.log([] + false - null + true); //NaN
+
+let y = 1;
+let x = y = 2; // x=y y=2
+alert(x); // x=2
+
+console.log([] + 1 + 2); // 12
+
+alert("1" [0]); //1
+
+console.log(2 && 1 && null && 0 && undefined); // остановиться и вернет null
+
+console.log(!!(a && b) === (a && b)); // !!( a && b ) - преобразуеться в true
+
+alert(null || 2 && 3 || 4); // запнеться и вернет 
+
+let a = [1, 2, 3];
+let b = [1, 2, 3]; // эти массивы не равны
+
+alert(+"Infinity"); //Infinity
+
+console.log("Ёжик" > "яблоко"); //false
+
+console.log(0 || "" || 2 || undefined || true || falsе); // 2
+*/
